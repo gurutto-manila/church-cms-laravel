a7f329622bee40aac2bda713454d8966305e932c
Gurutto Manila <125864240+gurutto-manila@users.noreply.github.com>
2026-04-08 14:57:02 +0800

    Update ChurchFactory.php

--- database/factories/ChurchFactory.php
+++ database/factories/ChurchFactory.php
@@ -1,25 +1,37 @@
 <?php
 
-use Faker\Generator as Faker;
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Arr;
+use App\Models\Church;
 
-$factory->define(App\Models\Church::class, function (Faker $faker) {
-	$mycities = array(
-		["cityname" => 31, "state" => 24],
-		["cityname" => 24, "state" => 24],
-		["cityname" => 32, "state" => 24],
-		["cityname" => 12, "state" => 12],
-		["cityname" => 15, "state" => 15],
-		["cityname" => 33, "state" => 15],
-	);
+class ChurchFactory extends Factory
+{
+    protected $model = Church::class;
 
-	$mycity = Arr::random($mycities);
-	
-    return [
-        //
-        'address'   => $faker->address,
-        'city_id'   => $mycity['cityname'],
-        'state_id'  => $mycity['state'],
-        'pincode'   => $faker->postcode,
-    ];
-});
+    public function definition(): array
+    {
+        $mycities = [
+            ["cityname" => 31, "state" => 24],
+            ["cityname" => 24, "state" => 24],
+            ["cityname" => 32, "state" => 24],
+            ["cityname" => 12, "state" => 12],
+            ["cityname" => 15, "state" => 15],
+            ["cityname" => 33, "state" => 15],
+        ];
+
+        $mycity = Arr::random($mycities);
+
+        return [
+            'name'      => fake()->company(),
+            'quotes'    => fake()->sentence(),
+            'address'   => fake()->address(),
+            'city_id'   => $mycity['cityname'],
+            'state_id'  => $mycity['state'],
+            'pincode'   => fake()->postcode(),
+            'slug'      => fake()->slug(),
+            'status'    => 1,
+        ];
+    }
+}
